e6189976ef46342ca90be3eb0802e619e77b46ab
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-05-27 21:12:26 +0100

    Add initial questions

---
+++ questions.py
@@ -0,0 +1,8 @@
+questions = [
+    "Would you rather fridge cold chocolate or room temperature chocolate?",
+    "At night do you worry more about someone breaking in or a kitchen fire?",
+    "Do you sometimes think there are snakes in the toilet bowl?",
+    "Would you rather drink toilet water or your own wee?",
+    "Do you prefer your ice cream all melty or all hard?",
+    "Burps or farts?",
+]
