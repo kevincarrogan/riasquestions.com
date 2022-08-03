61290fd06773ea30e5a952614d7962dd5f340c50
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-08-03 21:50:47 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -40,4 +40,5 @@ questions = [
     "Should we all go to the cinema to see Thor?",
     "Did you use the glass method? Did it proper scuttle about?",
     "Do you get a good vibe about the area? What do you like the most about it?",
+    "What's the most tragic thing that's happened to someone you know?",
 ]
